4954839f5ba9c7616a500a37465955177d4f878b
MiguelBrav <miguel_98-@hotmail.com>
2025-10-22 21:34:11 -0600

    update dotnet 8

--- GraphQLServer/Program.cs
+++ GraphQLServer/Program.cs
@@ -2,6 +2,7 @@ using GraphQLServer.Data;
 using GraphQLServer.Extensions;
 using GraphQLServer.GraphQl;
 using GraphQLServer.GraphQl.Publicaciones;
+using GraphQLServer.Helpers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using System.Reflection;
@@ -10,17 +11,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
 
-builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BlogConnection"),
+    x => x.MigrationsHistoryTable(HistoryRepository.DefaultTableName, "bg")));
+
 
 builder.Services.AddGraphQLServer()
     .AddQueryType()
     .AddMutationType()    
     .AddGraphQLServerTypes()
-    .RegisterDbContext<Context>()
     .AddProjections().AddFiltering().AddSorting();
 
-builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BlogConnection"),
-    x => x.MigrationsHistoryTable(HistoryRepository.DefaultTableName, "bg")));
+
 
 var app = builder.Build();
 
